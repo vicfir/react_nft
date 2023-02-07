e17584444c2ccd8a10f24cf71307a78f195a9101
vicfir <vicfir@hotmail.fr>
2023-02-07 16:58:03 +0100

    exo en courts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Main from './components/main/main';
+import Card from './components/main/main';
+
+import nftImg from '../src/components/main/public/img/image-equilibrium.jpg';
+import eth from '../src/components/main/public/img/icon-ethereum.svg';
 
 function App() {
   return (
     <div className="App">
-      <header>
-      </header>
-      <Main />
+      <main>
+        <Card _img={nftImg} _imgPrice={eth} _price={0.041+"ETH"}/>
+      </main>
     </div>
   );
 }
